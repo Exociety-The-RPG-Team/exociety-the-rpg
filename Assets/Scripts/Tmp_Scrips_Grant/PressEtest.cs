8ea33474a7cc86a592ca64b998c4eebb96eaeed9
Grant Gabel <grantg1016@gmail.com>
2024-09-11 21:22:27 -0400

    misc testing
    testing some misc 2d stuff and added trigger collision testing

---
+++ Assets/Scripts/Tmp_Scrips_Grant/PressEtest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressEtest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("pressed e down");
+        }
+	}
+}
